16a99a4ae4a1893f75aef80f0f984d669bcd33bd
mangadul <muin.abdul@gmail.com>
2017-01-07 10:45:20 +0700

    google analytic

--- application/views/depan.php
+++ application/views/depan.php
@@ -781,6 +781,17 @@ TRAVEL BANDUNG KEBUMEN (PP), TRAVEL BANDUNG TEMANGGUNG (PP), TRAVEL BANDUNG – 
     <!-- Theme JavaScript -->
     <script src="<?php echo base_url('assets/js/agency.min.js'); ?>"></script>
 
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-89937567-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVKVTVP"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
